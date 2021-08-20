8489bc38e8943a6e637e0ca7aee6adc29527f201
Razibul-Alam <Razibul-Alam@users.noreply.github.com>
2021-08-20 23:40:11 +0600

    new commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # mac-book-pro-Razibul-Alam
+
+Code Link https://github.com/programming-hero-web-course2/mac-book-pro-Razibul-Alam
+
---
+++ js/index.js
@@ -0,0 +1,24 @@
+
+function handleMemoryPrice(isIncrease) {
+    let memoryCost=document.getElementById('memory-cost')
+    
+    
+    if (isIncrease) {
+        memoryCost.innerText=80
+    }else{
+        memoryCost.innerText=0
+    }
+    
+}
+function handleMemoryPrice(isIncrease) {
+    let memoryText=document.getElementById('memory-cost')
+    
+    
+    if (isIncrease) {
+        memoryText.innerText=80
+    }else{
+        memoryText.innerText=0
+    }
+    
+}
+
